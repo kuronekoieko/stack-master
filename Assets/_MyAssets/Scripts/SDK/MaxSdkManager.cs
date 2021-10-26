adf5de4c29de0ee360a136ac5dff94b5dc2b86e7
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2021-10-26 22:56:59 +0900

    不要ログ削除

--- Assets/_MyAssets/Scripts/SDK/MaxSdkManager.cs
+++ Assets/_MyAssets/Scripts/SDK/MaxSdkManager.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class MaxSdkManager : MonoBehaviour
 {
-
     void Start()
     {
-        Debug.Log("テスト " + 0);
         MaxSdkCallbacks.OnSdkInitializedEvent += (MaxSdkBase.SdkConfiguration sdkConfiguration) =>
         {
-            Debug.Log("テスト " + 1);
             // AppLovin SDK is initialized, start loading ads
             MaxSdkInterstitial.i.InitializeInterstitialAds();
-            Debug.Log("テスト " + 2);
             MaxSdkBanner.i.InitializeBannerAds();
-
-            Debug.Log("テスト " + 3);
         };
-        Debug.Log("テスト " + 4);
         MaxSdk.SetSdkKey("nirDFR5Ia8FAlCZc9rlX8wP_kDNEiuHc4HGKHSQ4fFDhScOd5lW-T31uZdVJ9vqy35mpZCJVS7URsASrlXu7iF");
-        Debug.Log("テスト " + 5);
         // MaxSdk.SetUserId("USER_ID");
         MaxSdk.InitializeSdk();
-        Debug.Log("テスト " + 6);
         MaxSdk.SetMuted(true);  // オーディオのミュート
-        Debug.Log("テスト " + 7);
     }
 }
