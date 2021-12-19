61a3c8fb2f6403f05d8ab9d9eb20e759de87d990
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2021-12-20 00:00:50 +0900

    横移動の速度調整

--- Assets/_MyAssets/Scripts/Model/Variables.cs
+++ Assets/_MyAssets/Scripts/Model/Variables.cs
@@ -7,7 +7,7 @@ public class Variables : MonoBehaviour
 {
     public static ScreenState screenState = ScreenState.Game;
     public static float speedX = 6f;
-    public static float smoothTimeX = 0.05f;
+    public static float smoothTimeX = 0.01f;//0.05f
     public static float goalRate = 1.0f;
     public static bool isLaunchUIScene = false;
 }
