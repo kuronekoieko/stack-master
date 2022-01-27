f0f022b53ee97ae19073ce8dd2bab81d9100f8fe
松本勇吾 <>
2022-01-27 14:25:45 +0900

    障害物「プロペラ」追加、m035_vre2 作成

--- Assets/_MyAssets/Scripts/Game/Obstacles/PropellerController.cs
+++ Assets/_MyAssets/Scripts/Game/Obstacles/PropellerController.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class SpinningTopController : MonoBehaviour
+public class PropellerController : MonoBehaviour
 {
     [SerializeField] Transform body_transform;
 
     [Space(10)]
     [SerializeField] float rotateSpeed = 4;
-
     [Space(10)]
     [SerializeField] Vector3[] localPath_array;
     [SerializeField] float completeTime_sec = 1;
@@ -23,6 +22,6 @@ public class SpinningTopController : MonoBehaviour
 
     void FixedUpdate()
     {
-        body_transform.Rotate(0, rotateSpeed * Time.fixedDeltaTime / 0.02f, 0);
+        body_transform.Rotate(0, -rotateSpeed * Time.fixedDeltaTime / 0.02f, 0);
     }
 }
