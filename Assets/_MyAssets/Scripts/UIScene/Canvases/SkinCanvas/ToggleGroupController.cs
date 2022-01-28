07e9420fe0f98a41fb5ef6ab06f4b593a250d9cd
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2022-01-28 14:09:16 +0900

    新スキンの場合にスキン画面のマテリアルを開けないように

--- Assets/_MyAssets/Scripts/UIScene/Canvases/SkinCanvas/ToggleGroupController.cs
+++ Assets/_MyAssets/Scripts/UIScene/Canvases/SkinCanvas/ToggleGroupController.cs
@@ -11,18 +11,25 @@ public class ToggleGroupController : MonoBehaviour
         for (int i = 0; i < tabs.Length; i++)
         {
             var tab = tabs[i];
-            tab.toggle.onValueChanged.AddListener((isOn) =>
-            {
-                tab.tabObj.SetActive(isOn);
-                tab.toggle.image.color = isOn ? tab.toggle.colors.normalColor : tab.toggle.colors.disabledColor;
-            });
-
+            tab.toggle.onValueChanged.AddListener((isOn) => OnChangedToggleValue(tab, isOn));
             tab.tabObj.SetActive(tab.toggle.isOn);
             tab.toggle.image.color = tab.toggle.isOn ? tab.toggle.colors.normalColor : tab.toggle.colors.disabledColor;
         }
+
+        if (Variables.isSkinReal)
+        {
+            tabs[0].tabObj.SetActive(true);
+            tabs[0].toggle.gameObject.SetActive(false);
+            tabs[1].tabObj.SetActive(false);
+            tabs[1].toggle.gameObject.SetActive(false);
+        }
     }
 
-
+    void OnChangedToggleValue(Tab tab, bool isOn)
+    {
+        tab.tabObj.SetActive(isOn);
+        tab.toggle.image.color = isOn ? tab.toggle.colors.normalColor : tab.toggle.colors.disabledColor;
+    }
 }
 
 [System.Serializable]
